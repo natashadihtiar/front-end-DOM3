788541beeea005d058a9fb0d1994b97f2c3f6fb7
natashadihtiar <47831860+natashadihtiar@users.noreply.github.com>
2020-03-04 20:21:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const arr = [1,2,[1.1,1.2,1.3],3];//eslint-disable-line
-
+const array =[1,2,[1.1,1.2,1.3],3];//eslint-disable-line
 function generateList(array){//eslint-disable-line
     const $ul = document.createElement('ul');
     array.forEach(i => {
         const $li = document.createElement('li');
-        $ul.appendChild($li);
+        $ul.append($li);
         if (Array.isArray(i)) {
-            $li.innerHTML = generateList(i);
+            $li.outerHTML = generateList(i);
         } else {
             $li.textContent = i;
         }
     });
     return $ul.outerHTML;
 }
-document.body.outerHTML = generateList(arr);
-
+document.body.outerHTML = generateList(array);
